bc85e7668f66941b5b73369441fb1c358928c6cc
Osmond van Hemert <osmond@vanhemert.nu>
2019-06-26 15:13:20 +0200

    fix linting issue

--- example/fetchCustomer.js
+++ example/fetchCustomer.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const fetchCustomerById = async (db, customerId) => {
-  const {rows} = await db.query('SELECT firstname, lastname FROM customer WHERE id = $1', [customerId])
+  const { rows } = await db.query('SELECT firstname, lastname FROM customer WHERE id = $1', [customerId])
   return rows.map((r) => `${r.firstname} ${r.lastname}`)
 }
 
